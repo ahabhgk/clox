5d6a3aef60814bf134c0e6bcec1fbd61fbf6b6e3
ahabhgk <ahabhgk@gmail.com>
2021-12-27 15:05:11 +0800

    Initial Commit

---
+++ clox/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  clox
+//
+//  Created by ahabhgk on 2021/12/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
