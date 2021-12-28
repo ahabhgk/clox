26326243383980ef3ded341f52e8da69401a3478
ahabhgk <ahabhgk@gmail.com>
2021-12-28 21:36:38 +0800

    17: compiling expressions

--- vm.c
+++ vm.c
@@ -82,6 +82,18 @@ push(a op b); \
 }
 
 InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+    
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+    
+    InterpretResult result = run();
+    
+    freeChunk(&chunk);
+    return result;
 }
