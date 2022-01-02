ad589617d7ff24f978d88d2ab3d4651a68a17d66
ahabhgk <ahabhgk@gmail.com>
2022-01-02 16:49:24 +0800

    19: strings

--- memory.h
+++ memory.h
@@ -9,6 +9,12 @@
 #define clox_memory_h
 
 #include "common.h"
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+(type*)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 #define GROW_CAPACITY(capacity) \
 ((capacity) < 8 ? 8 : (capacity) * 2)
@@ -21,5 +27,6 @@ sizeof(type) * (newCount))
 reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif /* clox_memory_h */
--- vm.h
+++ vm.h
@@ -18,6 +18,7 @@ typedef struct {
     uint8_t* ip;
     Value stack[STACK_MAX];
     Value* stackTop;
+    Obj* objects;
 } VM;
 
 typedef enum {
@@ -25,6 +26,8 @@ typedef enum {
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void initVM();
 void freeVM();
